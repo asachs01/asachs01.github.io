7ad8474edb977698a718f166f64c82f9656111b6
Aaron Sachs <898627+asachs01@users.noreply.github.com>
2026-02-13 09:48:11 -0500

    chore: migrate custom domain from aaron.sachs.blog to sachsha.us

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://aaron.sachs.blog',
+  site: 'https://sachsha.us',
   integrations: [sitemap()],
 });
